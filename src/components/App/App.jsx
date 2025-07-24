f17033d1dbf887a7746d7f8ea7fb7e801fd8ad7d
DimitryGrig <dimasgrig1575@gmail.com>
2025-07-24 17:11:57 +0300

    fx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,53 +2,18 @@ import { useState, useRef, useEffect } from "react";
 import styles from "../../components/App/App.module.css";
 
 function App() {
-    const [value, setValue] = useState("");
-    const [bdisabled, setBDisabled] = useState(true);
-    const [errorText, setErrorText] = useState("");
-    const [listItems, setListItems] = useState([]);
-    const currentValueRef = useRef("");
-    const [date, setDate] = useState(new Date());
+  //  const [value, setValue] = useState("");
+  //  const [bdisabled, setBDisabled] = useState(true);
+
 
     useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
 
-        return () => clearInterval(intervalId);
     }, []);
 
     const onClick = () => {
-        const inputWord = prompt('Введите значение:');
-        if (inputWord && inputWord.length < 3) {
-            setErrorText("Введенное значение должно содержать минимум 3 символа");
-        } else if (inputWord) {
-            setValue(inputWord);
-            setBDisabled(false);
-            setErrorText("");
-            currentValueRef.current = inputWord;
-        }
+
     };
 
-    const dataformated = () => {
-        const day = String(date.getDate()).padStart(2, '0');
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const year = String(date.getFullYear());
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        const seconds = String(date.getSeconds()).padStart(2, '0');
-        return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
-    };
-
-    const onClickAddList = () => {
-        if (currentValueRef.current) {
-            setListItems(prevItems => [
-                ...prevItems,
-                currentValueRef.current + " " + dataformated(),
-            ]);
-            setValue("");
-            setBDisabled(true);
-        }
-    };
 
     return (
         <div className={styles.app}>
